ca2815bbee79673a61c3497f27b1de8bfd8e681f
Ewan Pigneret <153621886+EwanPigneret@users.noreply.github.com>
2025-03-31 10:42:09 +0200

    Update main.c
    Rewrote the detection of flags and added multiples files management

--- src/main.c
+++ src/main.c
@@ -1,37 +1,43 @@
 #include "my_ls.h"
 
+/**
+ * @summary
+ * @param
+ * @return
+ */
+
 int main (int argc, char** argv) {
     int index = 1;
-    char* flag = NULL;
+    int nb_files = 0;
+    char* flag = my_malloc("flag", sizeof(char) * 3);
     if (argc == 1) {  //./my_ls
         print_folder(".", 0);
     }
     else { 
         while (index < argc) {  // go throught all the arguments
             if (argv[index][0] == '-') {  // if the current arguments is a flag
-                if (argv[index + 1] != NULL) {  // checks if there are other parameters 
-                    if (argv[index + 1][0] == '-') {  // if the next one is a flag too
-                        my_malloc("flag", sizeof(char) * 3);
-                        flag = "-at";
-                        index += 2;
-                    }
-                    else {  // define flag as the current parameter
-                        flag = my_malloc("flag", sizeof(char) * my_strlen(argv[index]));
-                        flag = argv[index];
-                        index += 1;
-                    }
+                if (argv[index + 1] != NULL && argv[index + 1][0] == '-') {  // checks if there are other parameters 
+                    flag = "-at";
+                    index += 2;
                 }
                 else {  // define flag as the current parameter
-                    flag = my_malloc("flag", sizeof(char) * my_strlen(argv[index]));
                     flag = argv[index];
                     index += 1;
                 }
             }
-            if (flag != NULL) {  // if a flag is given
-                detect_flag(flag, argv[index]);
+            if (argv[index + 1] != NULL) {
+                printf("%s : \n", argv[index]);
             }
-            else {  // execute the rest of the code with the current fil
+            if (nb_files > 0) {
+                printf("%s : \n", argv[index]);
+            }
+            if (strcmp(flag, "\0")) {  // if a flag is given
+                detect_flag(flag, argv[index]);
+                nb_files += 1;
+            }
+            else {  // execute the rest of the code with the current file
                 print_folder(argv[index], 0);
+                nb_files += 1;
             }
             index += 1;
         }
