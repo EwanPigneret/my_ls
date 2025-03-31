985303e7adf2cf8304b3818aa3359d4d211442b9
Ewan Pigneret <153621886+EwanPigneret@users.noreply.github.com>
2025-03-31 10:37:48 +0200

    Update compare_letter.c
    Adding comment on the function

--- src/compare_letter.c
+++ src/compare_letter.c
@@ -1,6 +1,12 @@
 #include "my_ls.h"
 
-int compare_letter(listnode* current, listnode* next) {  // return if two files/directory are in alphabetical order
+/**
+ * @summary Return if two files/directory are in alphabetical order
+ * @param listnode* current, listnode* next - Node that is compared to the next one
+ * @return -1 if current < next, 1 if current > next
+ */
+
+int compare_letter(listnode* current, listnode* next) {  
     int index = 0;
     while (current->name[index] != '\0' && next->name[index] != '\0' && current->name[index] == next->name[index]) {  // go throught the 2 strings until it's the end of one of them or the two letters aren't the same
         index += 1;
