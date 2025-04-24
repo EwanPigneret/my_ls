afad563beeb8f402d0bf839d4ea73c722b76a2fa
Ewan Pigneret <153621886+EwanPigneret@users.noreply.github.com>
2025-04-24 14:48:55 +0200

    FEAT : final commit

--- include/my_ls.h
+++ include/my_ls.h
@@ -13,27 +13,26 @@
 typedef struct s_listnode
 {
     char* name;
-    long last_modif;
+    struct stat* last_modif;
     struct s_listnode* next;
 } listnode;
 #endif
 
-listnode* add_end(listnode*, char*);
-listnode* add_head(listnode*, char*);
-listnode* add_node(listnode*, listnode*, int (*cmp_func)(listnode*, listnode*));
-char* binary_to_text(int);
+listnode* add_end(listnode*, char*, struct stat*);
+listnode* add_head(listnode*, char*, struct stat*);
+listnode* add_node(listnode*, listnode*, int (*cmp_func)(listnode*, listnode*), struct stat*);
 int compare_letter(listnode*, listnode*);
 int compare_modification(listnode*, listnode*);
-char* concatenation_string(char*, char*);
+char* concatenation_string(char*, char*, char*);
 int detect_flag(char*);
 listnode* init_head(char*, char*);
 listnode* insert_element(listnode*, char*, char*);
 int linked_list_length(listnode*);
-listnode* linked_list_sort (listnode*, int (*cmp_func) (listnode*, listnode*), char*);
+listnode* linked_list_sort (listnode*, int (*cmp_func) (listnode*, listnode*));
 int main(int, char**);
 void* my_malloc(char*, size_t);
 int my_strlen(char*);
-void print_folder(listnode*, int, char*);
+void print_folder(listnode*, int);
 void print_file(char*, int);
 void print_linked_list(listnode*);
-listnode* sort_folder(listnode*, int, char*);
+listnode* sort_folder(listnode*, int);
