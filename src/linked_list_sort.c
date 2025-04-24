91eef4e30cd11c1b8f411b961b2e27fd02040d56
Ewan Pigneret <153621886+EwanPigneret@users.noreply.github.com>
2025-04-24 14:52:17 +0200

    FEAT : final commit

--- src/linked_list_sort.c
+++ src/linked_list_sort.c
@@ -7,7 +7,7 @@
  * @result Returns the modified linked list
  */
 
-listnode* linked_list_sort(listnode* head, int (*cmp_func)(listnode*, listnode*), char* path)
+listnode* linked_list_sort(listnode* head, int (*cmp_func)(listnode*, listnode*))
 {
     listnode* current = head;
     // checks for error
@@ -15,18 +15,18 @@ listnode* linked_list_sort(listnode* head, int (*cmp_func)(listnode*, listnode*)
     {
         current = current->next;
     }
-    listnode* new_head = NULL;
+    listnode* new_head = (listnode*)my_malloc("head", sizeof(listnode));
+    new_head->name = current->name;
+    new_head->last_modif = current->last_modif;
     listnode* new_current = new_head;
     // initialise the first element of the list
-    new_head = init_head(current->name, path);
-    new_current = new_head;
     current = current->next;
     while (current != NULL)
     {
         // the element is smaller than the head
         if (cmp_func(current, new_head) == -1)
         {
-            new_head = add_head(new_head, current->name);
+            new_head = add_head(new_head, current->name, current->last_modif);
             new_current = new_head;
         }
         else
@@ -42,12 +42,12 @@ listnode* linked_list_sort(listnode* head, int (*cmp_func)(listnode*, listnode*)
                 // the element should be at the end of the list
                 if (new_current->next == NULL)
                 {
-                    new_head = add_end(new_head, current->name);
+                    new_head = add_end(new_head, current->name, current->last_modif);
                 }
                 // the element should be added in the middle of the list
                 else
                 {
-                    new_head = add_node(new_head, current, cmp_func);
+                    new_head = add_node(new_head, current, cmp_func, current->last_modif);
                 }
             }
         }
