ec6c7fd693eab412a053f585cdb8bb96af8f11d6
Ewan Pigneret <153621886+EwanPigneret@users.noreply.github.com>
2025-04-24 14:51:35 +0200

    FEAT : final commit

--- src/init_head.c
+++ src/init_head.c
@@ -10,13 +10,17 @@ listnode* init_head(char* head_name, char* path)
 {
     struct stat* stat_head = (struct stat*)my_malloc("stat_head", sizeof(struct stat));
     listnode* head = (listnode*)my_malloc("head", sizeof(listnode));
-    if (stat(concatenation_string(path, head_name), stat_head) == -1)
+    head->name = head_name;
+    if (head_name[0] != '/')
+    {
+        head_name = concatenation_string(path, head_name, head_name);
+    }
+    if (stat(head_name, stat_head) == -1)
     {
         perror("stat head");
         exit(EXIT_FAILURE);
     }
-    head->last_modif = stat_head->st_mtim.tv_nsec;
-    head->name = head_name;
+    head->last_modif = stat_head;
     head->next = NULL;
     return head;
 }
