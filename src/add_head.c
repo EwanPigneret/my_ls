9dec6c5206aea9d09a2f6c14f8ca33d9173ed96f
Ewan Pigneret <153621886+EwanPigneret@users.noreply.github.com>
2025-04-24 14:49:50 +0200

    FEAT : final commit

--- src/add_head.c
+++ src/add_head.c
@@ -7,11 +7,12 @@
  * @return The head of the modified list
  */
 
-listnode* add_head(listnode* head, char* name)
+listnode* add_head(listnode* head, char* name, struct stat* modif_head)
 {
     listnode* newhead = NULL;
     newhead = (listnode*)my_malloc("newnode", sizeof(listnode));
     newhead->name = name;
+    newhead->last_modif = modif_head;
     newhead->next = head;
     head = newhead;
     return head;
